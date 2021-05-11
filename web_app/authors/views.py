080f4aa65e3bb882facc9b34c808e205f97f5c26
leulens <leulens@yandex.ru>
2021-05-11 12:03:38 +0500

    working with apps / login+registration

--- web_app/authors/views.py
+++ web_app/authors/views.py
@@ -7,3 +7,4 @@ def author_profile_datail(request, **kwargs):
     user = Author.objects.get(user__username=kwargs.get('username'))
     context = {'user':user}
     return render(request, 'authors/base-author.html', context)
+
